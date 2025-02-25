088ca3f17c4c300668ff1d39883ee110f9a9fe27
kashanaqeel <kashanaqeel74@gmail.com>
2025-02-25 18:45:17 +0500

    fix(status): changed status complete requested to patch instead of put

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", todoController.getAllTodos);
 router.post("/", todoController.createTodo);
 router.put("/:id", todoController.updateTodo);
-router.put("/:id/completed", todoController.markAsCompleted);
+router.patch("/:id", todoController.markAsCompleted);
 router.delete("/:id", todoController.deleteTodo);
 router.delete("/", todoController.deleteAllTodos);
 
